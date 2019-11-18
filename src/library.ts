9b9f74ec9bec871092312acb51cb13a922144e23
David Castro <arimus@gmail.com>
2019-11-17 19:58:58 -0700

    Provide main entrypoint and generated typings to use the core as a library

---
+++ src/library.ts
@@ -0,0 +1,6 @@
+export * from "./AbstractNamingStrategy";
+export * from "./Engine";
+export * from "./IGenerationOptions";
+export * from "./IConnectionOptions";
+export * from "./NamingStrategy";
+export * from "./Utils";
