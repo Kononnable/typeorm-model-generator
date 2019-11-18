e99e337fca2cade4f483c57f657c9eccff29c46c
David Castro <arimus@gmail.com>
2019-11-18 14:55:39 -0700

    fix for linting issue causing build checks to fail

--- src/library.ts
+++ src/library.ts
@@ -1,21 +1,22 @@
+import RelationInfo from "./models/RelationInfo";
+import ColumnInfo from "./models/ColumnInfo";
+import IndexColumnInfo from "./models/IndexColumnInfo";
+import IndexInfo from "./models/IndexInfo";
+import EntityInfo from "./models/EntityInfo";
+import RelationTempInfo from "./models/RelationTempInfo";
+
+// models exports - there may be a more succinct way to export default classes,
+// but this was the only one I could find that seemed happy
+export type RelationInfo = RelationInfo;
+export type ColumnInfo = ColumnInfo;
+export type IndexColumnInfo = IndexColumnInfo;
+export type IndexInfo = IndexInfo;
+export type EntityInfo = EntityInfo;
+export type RelationTempInfo = RelationTempInfo;
+
 export * from "./AbstractNamingStrategy";
 export * from "./Engine";
 export * from "./IGenerationOptions";
 export * from "./IConnectionOptions";
 export * from "./NamingStrategy";
 export * from "./Utils";
-
-// models exports - there may be a more succinct way to export default classes,
-// but this was the only one I could find that seemed happy
-import RelationInfo from "./models/RelationInfo";
-export type RelationInfo = RelationInfo;
-import ColumnInfo from "./models/ColumnInfo";
-export type ColumnInfo = ColumnInfo;
-import IndexColumnInfo from "./models/IndexColumnInfo";
-export type IndexColumnInfo = IndexColumnInfo;
-import IndexInfo from "./models/IndexInfo";
-export type IndexInfo = IndexInfo;
-import EntityInfo from "./models/EntityInfo";
-export type EntityInfo = EntityInfo;
-import RelationTempInfo from "./models/RelationTempInfo";
-export type RelationTempInfo = RelationTempInfo;
