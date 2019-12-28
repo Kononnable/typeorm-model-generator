0e3d766e7383ce667d81028fd13cafd5a4b50a22
Kononnable <kononnable@gmail.com>
2019-12-28 12:01:52 +0100

    avoid name colisions in tomg exports

--- src/library.ts
+++ src/library.ts
@@ -1,19 +1,18 @@
-import { Column as ColumnModel } from "./models/Column";
-import { Entity as EntityModel } from "./models/Entity";
-import { Index as IndexModel } from "./models/Index";
-import { Relation as RelationModel } from "./models/Relation";
-import { RelationId as RelationIdModel } from "./models/RelationId";
+import * as Engine from "./Engine";
+import * as IConnectionOptions from "./IConnectionOptions";
+import * as IGenerationOptions from "./IGenerationOptions";
+import * as NamingStrategy from "./NamingStrategy";
+import * as Utils from "./Utils";
 
-// models exports - there may be a more succinct way to export default classes,
-// but this was the only one I could find that seemed happy
-export type Column = ColumnModel;
-export type Entity = EntityModel;
-export type Index = IndexModel;
-export type Relation = RelationModel;
-export type RelationId = RelationIdModel;
-
-export * from "./Engine";
-export * from "./IConnectionOptions";
-export * from "./IGenerationOptions";
-export * from "./NamingStrategy";
-export * from "./Utils";
+export { Column } from "./models/Column";
+export { Entity } from "./models/Entity";
+export { Index } from "./models/Index";
+export { Relation } from "./models/Relation";
+export { RelationId } from "./models/RelationId";
+export {
+    Engine,
+    IConnectionOptions,
+    IGenerationOptions,
+    NamingStrategy,
+    Utils
+};
