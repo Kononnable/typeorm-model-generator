ac6695123be8e64f4b51470a4be8505bbda30637
Frederik Hummel <frederik.hummel@bumbledev.com>
2020-10-03 11:44:59 +0200

    Make instanceName optional

--- src/IConnectionOptions.ts
+++ src/IConnectionOptions.ts
@@ -15,7 +15,7 @@ export default interface IConnectionOptions {
         | "oracle"
         | "sqlite";
     schemaName: string;
-    instanceName: string;
+    instanceName?: string;
     ssl: boolean;
     skipTables: string[];
     onlyTables: string[];
@@ -30,7 +30,7 @@ export function getDefaultConnectionOptions(): IConnectionOptions {
         password: "",
         databaseType: undefined as any,
         schemaName: "",
-        instanceName: undefined as any,
+        instanceName: undefined,
         ssl: false,
         skipTables: [],
         onlyTables: [],
